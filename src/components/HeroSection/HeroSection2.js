adfc09508d8bbcb45dfea0b263afe6dbe831b168
faiza-zakir <faiza.prism@gmail.com>
2022-03-22 14:56:44 +0500

    add zoho form to banner

--- src/components/HeroSection/HeroSection2.js
+++ src/components/HeroSection/HeroSection2.js
@@ -3,13 +3,10 @@ import { connect } from "react-redux";
 import PopupForm from "../popup/PopupForm.js";
 import headerBackgroundImage from "./../../assets/Images/LandingPage/Top-Section-Background.png";
 import "./HerroSection2.scss";
-import { Form } from "react-bootstrap";
-import axios from 'axios';
-import Hidden from '@material-ui/core/Hidden';
-
-import ModalVideo from "react-modal-video";
 
 import { language } from "./../../data/language.js";
+
+import ZohoBannerForm from "../ZohoBannerForm/ZohoBannerForm.js"
 
 
 
@@ -17,50 +14,9 @@ class HeroSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
       show: false,
-      name: '',
-      phone_number: "",
-      email: "",
-      openVideo: false
-
     };
   }
-
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
-  }
-
-  handlePhoneChange = event => {
-    this.setState({ phone_number: event.target.value });
-  }
-
-  handleEmailChange = event => {
-    this.setState({ email: event.target.value });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-
-    let name = this.state.name;
-    let phone_number = this.state.phone_number;
-    let email = this.state.email;
-
-
-    axios.post(`https://prismcloudhosting.com/BUHEN/Apis/public/api/buhen-form`, { name, phone_number, email })
-      .then(res => {
-        if (res.status === 200 || res.status === 201) {
-          //alert(res.data.message);
-
-          window.location =
-            "thankyou.html";
-        }
-        console.log(res);
-        console.log(res.data);
-      })
-  }
-
 
   handleClose = () => {
 
@@ -85,12 +41,6 @@ class HeroSection extends React.Component {
           // }}
           id="top-banner-form"
         >
-          {/* <a
-            href="https://www.youtube.com/watch?v=6R9xTxZxZHA"
-            className="popup-youtube video-play-icon d-inline-block"
-          >
-            <BsFillPlayFill className="rozella-play-btn" />
-          </a> */}
           <div className="container">
             <div
               className={`row align-items-center justify-content-between banner-section-row ${this.props.activeLanguage === "arabic"
@@ -118,152 +68,19 @@ class HeroSection extends React.Component {
                   <button class="button btn solid-btn english header_cta_mobile hero_banner_button" id="submit-footer"
                     onClick={this.handleShow}
                   >Submit</button>
-                  <Hidden mdUp>
-                    <PopupForm
-
-                      show={this.state.show}
-
-                      onHide={this.handleClose}
-
-                    />
-                  </Hidden>
+                  <PopupForm
+                    show={this.state.show}
+                    onHide={this.handleClose}
+                  />
                 </div>
               </div>
-              <div className="col-md-6 col-lg-5 d-none d-sm-block">
-                {/* <div
-                  className="hero-animation-img banner-image"
-                  id="banner-form"
-                > */}
-                <Form
-                  onSubmit={this.handleSubmit}
-                  className="banner-form"
-                >
-                  {/* <div className="align-items-center input-button-wrap"> */}
-
-                  <h2 className="formheading">Contact Us</h2>
-                  <div className="row">
-                    <div className="col-md-6 d-flex align-items-center ">
-                      <label for="name">First Name <span className="text-danger"><sup>*</sup></span></label>
-                    </div>
-                    <div className="col-md-6">
-                      <input
-                        type="text"
-                        className="form-control input"
-                        id="name"
-                        name="name"
-                        // placeholder="Full Name"
-                        required="required"
-                        // value={isSubscribe.email}
-                        // onChange={handleOnChange}
-                        onChange={this.handleNameChange}
-                      />
-                    </div>
-                    <div className="col-md-6 d-flex align-items-center ">
-                      <label for="lname">Last Name <span className="text-danger"><sup>*</sup></span></label>
-                    </div>
-                    <div className="col-md-6">
-                      <input
-                        type="text"
-                        className="form-control input"
-                        id="lname"
-                        name="lname"
-                        // placeholder="Full Name"
-                        required="required"
-                        // value={isSubscribe.email}
-                        // onChange={handleOnChange}
-                        onChange={this.handleNameChange}
-                      />
-                    </div>
-                    <div className="col-md-6 d-flex align-items-center ">
-                      <label for="email">Email <span className="text-danger"><sup>*</sup></span></label>
-                    </div>
-                    <div className="col-md-6">
-                      <input
-                        type="email"
-                        className="form-control input"
-                        id="email"
-                        name="email"
-                        // placeholder="Email Address"
-                        required="required"
-                        // value={isSubscribe.email}
-                        // onChange={handleOnChange}
-                        onChange={this.handleEmailChange}
-                      />
-                    </div>
-                    <div className="col-md-6 d-flex align-items-center ">
-                      <label for="phone_number">Phone <span className="text-danger"><sup>*</sup></span></label>
-                    </div>
-                    <div className="col-md-6">
-                      <input
-                        type="text"
-                        className="form-control input"
-                        id="phone_number"
-                        name="phone_number"
-                        // placeholder="Phone Number"
-                        required="required"
-                        // value={isSubscribe.email}
-                        // onChange={handleOnChange}
-                        onChange={this.handlePhoneChange}
-                      />
-                    </div>
-                    <div className="col-md-6 d-flex align-items-center ">
-                      <label for="budget">Budget <span className="text-danger"><sup>*</sup></span></label>
-                    </div>
-                    <div className="col-md-6">
-                      <input
-                        type="text"
-                        className="form-control input"
-                        id="budget"
-                        name="budget"
-                        // placeholder="Phone Number"
-                        required="required"
-                        // value={isSubscribe.email}
-                        // onChange={handleOnChange}
-                        onChange={this.handlePhoneChange}
-                      />
-                    </div>
-                    <div className="col-md-6 d-flex align-items-center ">
-                      <label for="lookig_for">What are you looking for? <span className="text-danger"><sup>*</sup></span></label>
-                    </div>
-                    <div className="col-md-6">
-                      <input
-                        type="text"
-                        className="form-control input"
-                        id="lookig_for"
-                        name="lookig_for"
-                        // placeholder="Phone Number"
-                        required="required"
-                        // value={isSubscribe.email}
-                        // onChange={handleOnChange}
-                        onChange={this.handlePhoneChange}
-                      />
-                    </div>
-                  </div>
-                  <center>
-                    <button className="button btn solid-btn english hero_banner_button mt-4">
-                      Submit
-                    </button>
-                  </center>
-
-                  {/* </div> */}
-                </Form>
-                {/* <iframe frameborder="0" style={{ height: "500px", width: "99%", border: "none" }} src='https://forms.zohopublic.com/faizaprism/form/ContactUs1/formperma/PX_fvY9aiv-U4p41GOmPU0NpBsuRXuJdG9gNqy8nNK0'></iframe> */}
-                {/* </div> */}
+              <div className="col-md-6 col-lg-5 zohoFormDisplayOnDesktop">
+                <ZohoBannerForm />
               </div>
             </div>
 
           </div>
-          <ModalVideo
-            channel="youtube"
-            autoplay={1}
-            isOpen={this.openVideo}
-            videoId="6R9xTxZxZHA"
-            onClose={false}
-          />
         </section>
-
-
-
 
       </React.Fragment>
     );
--- src/components/popup/PopupForm.js
+++ src/components/popup/PopupForm.js
@@ -1,205 +1,29 @@
-import React, { useEffect } from "react";
-import { Modal, Button, Row, Col, Form, Container } from "react-bootstrap";
-import { MdClose } from "react-icons/md";
+import React from "react";
+import { Modal, Container } from "react-bootstrap";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import axios from 'axios';
+
+import ZohoBannerForm from "../ZohoBannerForm/ZohoBannerForm.js"
 
 
-// const PopupForm = (props) => {
-class PopupForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            phone_number: "",
-            email: ""
-            // designation: "",
-            // company: ""
 
-        };
-    }
-
-    handleNameChange = event => {
-        this.setState({ name: event.target.value });
-    }
-
-    handlePhoneChange = event => {
-        this.setState({ phone_number: event.target.value });
-    }
-
-    handleEmailChange = event => {
-        this.setState({ email: event.target.value });
-    }
-
-
-    // handleDesignationChange = event => {
-    //     this.setState({ designation: event.target.value });
-    // }
-
-    // handleCompanyChange = event => {
-    //     this.setState({ company: event.target.value });
-    // }
-
-    handleSubmit = event => {
-        event.preventDefault();
-
-        let name = this.state.name;
-        let email = this.state.email;
-        let phone_number = this.state.phone_number;
-        let company = this.state.company;
-        let designation = this.state.designation;
-
-        // const user = this.state
-        // debugger;
-        axios.post(`https://prismcloudhosting.com/BUHEN/Apis/public/api/buhen-form`, { name, email, phone_number })
-            .then(res => {
-                if (res.status === 200 || res.status === 201) {
-                    alert(res.data.message);
-                }
-                console.log(res);
-                console.log(res.data);
-            })
-    }
-
-    render() {
-
-        return (
-            <Modal
-                {...this.props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-                className="Documents"
-                style={{ paddingLeft: "0px" }}
-            >
-                <Container>
-                    <Row>
-                        <Col sm="auto">
-                            <span className={"modalIcon"} onClick={this.props.onHide}>
-                                <ArrowBackIcon />
-                            </span>
-                        </Col>
-                        <Col sm>
-                            <Modal.Body>
-                                <Form
-                                    // method="post"
-                                    onSubmit={this.handleSubmit}
-                                    className="banner-form"
-                                >
-                                    <h2 className="formheading">Contact Us</h2>
-                                    <div className="row">
-                                        <div className="col-md-6 d-flex align-items-center ">
-                                            <label for="name">First Name <span className="text-danger"><sup>*</sup></span></label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <input
-                                                type="text"
-                                                className="form-control input"
-                                                id="name"
-                                                name="name"
-                                                // placeholder="Full Name"
-                                                required="required"
-                                                // value={isSubscribe.email}
-                                                // onChange={handleOnChange}
-                                                onChange={this.handleNameChange}
-                                            />
-                                        </div>
-                                        <div className="col-md-6 d-flex align-items-center ">
-                                            <label for="lname">Last Name <span className="text-danger"><sup>*</sup></span></label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <input
-                                                type="text"
-                                                className="form-control input"
-                                                id="lname"
-                                                name="lname"
-                                                // placeholder="Full Name"
-                                                required="required"
-                                                // value={isSubscribe.email}
-                                                // onChange={handleOnChange}
-                                                onChange={this.handleNameChange}
-                                            />
-                                        </div>
-                                        <div className="col-md-6 d-flex align-items-center ">
-                                            <label for="email">Email <span className="text-danger"><sup>*</sup></span></label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <input
-                                                type="email"
-                                                className="form-control input"
-                                                id="email"
-                                                name="email"
-                                                // placeholder="Email Address"
-                                                required="required"
-                                                // value={isSubscribe.email}
-                                                // onChange={handleOnChange}
-                                                onChange={this.handleEmailChange}
-                                            />
-                                        </div>
-                                        <div className="col-md-6 d-flex align-items-center ">
-                                            <label for="phone_number">Phone <span className="text-danger"><sup>*</sup></span></label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <input
-                                                type="text"
-                                                className="form-control input"
-                                                id="phone_number"
-                                                name="phone_number"
-                                                // placeholder="Phone Number"
-                                                required="required"
-                                                // value={isSubscribe.email}
-                                                // onChange={handleOnChange}
-                                                onChange={this.handlePhoneChange}
-                                            />
-                                        </div>
-                                        <div className="col-md-6 d-flex align-items-center ">
-                                            <label for="budget">Budget <span className="text-danger"><sup>*</sup></span></label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <input
-                                                type="text"
-                                                className="form-control input"
-                                                id="budget"
-                                                name="budget"
-                                                // placeholder="Phone Number"
-                                                required="required"
-                                                // value={isSubscribe.email}
-                                                // onChange={handleOnChange}
-                                                onChange={this.handlePhoneChange}
-                                            />
-                                        </div>
-                                        <div className="col-md-6 d-flex align-items-center ">
-                                            <label for="lookig_for">What are you looking for? <span className="text-danger"><sup>*</sup></span></label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <input
-                                                type="text"
-                                                className="form-control input"
-                                                id="lookig_for"
-                                                name="lookig_for"
-                                                // placeholder="Phone Number"
-                                                required="required"
-                                                // value={isSubscribe.email}
-                                                // onChange={handleOnChange}
-                                                onChange={this.handlePhoneChange}
-                                            />
-                                        </div>
-                                    </div>
-                                    <center>
-                                        <button className="button btn solid-btn english hero_banner_button mt-4">
-                                            Submit
-                                        </button>
-                                    </center>
-
-                                </Form>
-                            </Modal.Body>
-                        </Col>
-                    </Row>
-                </Container>
-            </Modal>
-        );
-    };
-
-}
+const PopupForm = (props) => {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            className="Documents"
+            style={{ paddingLeft: "0px" }}
+        >
+            <Container>
+                <span className={"modalIcon"} onClick={props.onHide}>
+                    <ArrowBackIcon />
+                </span>
+                <ZohoBannerForm />
+            </Container>
+        </Modal>
+    );
+};
 
 export default PopupForm;
